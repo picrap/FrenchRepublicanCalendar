2a88031cef0ff9eed896e8693f2b00c315bdc664
Pascal Craponne <pascal@craponne.fr>
2019-01-06 11:57:27 +0100

    Added templates & appveyor build

---
+++ FrenchRepublicanCalendar/Properties/ProductInfo.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyDescription("Vive la révolution! A few classes to convert from and to French republican calendar (that existed between 1792 and 1806).")]
+[assembly: AssemblyCompany("picrap")]
+[assembly: AssemblyProduct("FrenchRepublicanCalendar")]
+[assembly: AssemblyCopyright("MIT license https://raw.githubusercontent.com/picrap/FrenchRepublicanCalendar/master/LICENSE")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")] // 
