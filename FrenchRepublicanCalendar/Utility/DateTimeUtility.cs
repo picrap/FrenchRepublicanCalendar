46a36f6d3cd844ebde15b0159a5d3100e7860c2b
Pascal Craponne <pascal@craponne.fr>
2019-01-06 12:38:29 +0100

    Added missing xml-doc & upgraded package

--- FrenchRepublicanCalendar/Utility/DateTimeUtility.cs
+++ FrenchRepublicanCalendar/Utility/DateTimeUtility.cs
@@ -3,16 +3,29 @@ namespace FrenchRepublicanCalendar.Utility
 {
     using System;
 
+    /// <summary>
+    /// Utilities to <see cref="DateTime"/>
+    /// </summary>
     public static class DateTimeUtility
     {
         // https://stackoverflow.com/a/5254812/67004
         private const double JulianDateStart = 2415018.5;
 
+        /// <summary>
+        /// Converts a <see cref="DateTime"/> to Julian date
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
         public static double ToJulianDate(this DateTime date)
         {
             return date.ToOADate() + JulianDateStart;
         }
 
+        /// <summary>
+        /// Creates a <see cref="DateTime"/> from Julian date
+        /// </summary>
+        /// <param name="jd"></param>
+        /// <returns></returns>
         public static DateTime FromJulianDate(double jd)
         {
             return DateTime.FromOADate(jd - JulianDateStart);
