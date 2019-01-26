2eba6080251d25649539cddc4b89ec0701942f8a
Pascal Craponne <pascal@craponne.fr>
2019-01-26 21:50:42 +0100

    Also targetting .NET 4.0

--- FrenchRepublicanCalendar/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// It's the French republican calendar!
-// https://github.com/picrap/FrenchRepublicanCalendar
-// Released under MIT license
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("French Republican Calendar")]
