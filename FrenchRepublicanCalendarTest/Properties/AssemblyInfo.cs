f33d461fd4acb3010c985b418c4adda9bfff9bdf
pascal <pascal@craponne.fr>
2024-07-26 22:51:10 +0200

    Upgraded versions and dependencies

--- FrenchRepublicanCalendarTest/Properties/AssemblyInfo.cs
+++ FrenchRepublicanCalendarTest/Properties/AssemblyInfo.cs
@@ -4,20 +4,9 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CalendarTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CalendarTest")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("2d2220ac-59bc-4cec-9fc1-a31f7182c3a6")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
